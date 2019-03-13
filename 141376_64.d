6110fec5336decb4d26d1e9649592478661896d6
philippe <ppeltriaux@netcourier.com>
2019-03-13 19:20:01 -0400

    Remise en page du diagramme de classes

--- 141376_64.d
+++ 141376_64.d
@@ -2,71 +2,71 @@ format 221
 
 classcanvas 128064 class_ref 128192 // BDD
   classdiagramsettings member_max_width 0 end
-  xyz 10 155.5 2000
+  xyz 10 155.3 2000
 end
 classcanvas 128192 class_ref 134976 // EvaluationTP
   classdiagramsettings member_max_width 0 end
-  xyz 448.6 62.9 2000
+  xyz 448.4 62.9 2000
 end
 classcanvas 128320 class_ref 135104 // IhmNoter
   classdiagramsettings member_max_width 0 end
-  xyz 245 705.1 2000
+  xyz 245.6 705.1 2000
 end
 classcanvas 128448 class_ref 134848 // IhmAccueil
   classdiagramsettings member_max_width 0 end
-  xyz 33.1 707.6 2005
+  xyz 33.5 707.2 2005
 end
 classcanvas 128960 class_ref 135232 // IhmConsulterNotes
   classdiagramsettings member_max_width 0 end
-  xyz 414.5 706 2000
+  xyz 414.7 706.4 2000
 end
 classcanvas 129216 class_ref 135488 // IhmCreerClasse
   classdiagramsettings member_max_width 0 end
-  xyz 743.4 228.6 2000
+  xyz 743.8 228.6 2000
 end
 classcanvas 129344 class_ref 135360 // SystemeFichiers
   classdiagramsettings member_max_width 0 end
-  xyz 10.9 57.5 2000
+  xyz 10.3 57.3 2000
 end
 classcanvas 129728 class_ref 142016 // IhmCreerDevoir
   classdiagramsettings member_max_width 0 end
-  xyz 935.3 258.4 2000
+  xyz 935.3 258.2 2000
 end
 classcanvas 130752 class_ref 148544 // IhmCreerCritere
   classdiagramsettings member_max_width 0 end
-  xyz 755.5 48.6 2000
+  xyz 755.7 48.2 2000
 end
 classcanvas 131264 class_ref 148672 // IhmModifierClasse
   classdiagramsettings member_max_width 0 end
-  xyz 890 44.3 2006
+  xyz 890.8 44.3 2006
 end
 classcanvas 131520 class_ref 148800 // IhmSupprimerClasse
   classdiagramsettings member_max_width 0 end
-  xyz 568.3 706 2000
+  xyz 568.5 706.4 2000
 end
 classcanvas 131776 class_ref 148928 // IhmModiferSupprimerCritere
   classdiagramsettings member_max_width 0 end
-  xyz 731.2 707.6 2000
+  xyz 731.4 707 2000
 end
 classcanvas 132160 class_ref 149056 // IhmParametrer
   classdiagramsettings member_max_width 0 end
-  xyz 742 471.2 2000
+  xyz 742.4 471.6 2000
 end
 packagecanvas 132544 
   package_ref 134720 // modeles
-   color green  xyzwh 6.6 6.8 1995 195 512
+   color green  xyzwh 6.4 7 1995 195 512
 end
 packagecanvas 132672 
   package_ref 134848 // controleur
-   color blue  xyzwh 428 7 1985 239 656
+   color blue  xyzwh 427.6 7.4 1985 239 656
 end
 packagecanvas 132800 
   package_ref 134592 // vues
-   color lightmagenta  xyzwh 732 1 1995 353 619
+   color lightmagenta  xyzwh 731.2 1 1995 353 619
 end
 packagecanvas 132928 
   package_ref 134592 // vues
-   color lightmagenta  xyzwh 25 658 1990 936 336
+   color lightmagenta  xyzwh 25.2 658 1990 936 336
 end
 relationcanvas 128576 relation_ref 128064 // <aggregation>
   decenter_end 578
